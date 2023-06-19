8eef907464a0d35b0987f1bba79492b8d0b994e8
riskalaluna <riskalaluna@gmail.com>
2023-06-19 14:57:47 +0700

    melakukan perulangan setiap garis

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -30,4 +30,13 @@ int main() {
 	ifstream infile;
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+
+	}
 }
