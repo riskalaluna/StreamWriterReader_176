8c11c6991ba59681abcb832c4f88f909a926ebf5
riskalaluna <riskalaluna@gmail.com>
2023-06-19 15:45:52 +0700

    selesai

--- exeptionStandar/exeptionStandar.cpp
+++ exeptionStandar/exeptionStandar.cpp
@@ -19,5 +19,7 @@ int main()
 		cout << e.what() << endl;
 		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
 	}
-
+	cout << "Baris Program yang terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
 }
